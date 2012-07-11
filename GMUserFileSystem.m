f4ccead0c3afaa29a64403029c652c0b026cfbe3
Benjamin Fleischer <fleiben@gmail.com>
2012-07-11 21:42:37 +0200

    Use [[NSFileManager alloc] init] instead of [NSFileManager defaultManager]
    Using the singleton default file manager is not recommended because it is not
    considered thread safe. Creating a specific file manager is supported since Mac
    OS X 10.5.
    
    Patch by @ChrisJEChandler
    
    Conflicts:
    	GMUserFileSystem.m

--- GMUserFileSystem.m
+++ GMUserFileSystem.m
@@ -2361,8 +2361,8 @@ static struct fuse_operations fusefm_oper = {
   // NSFileManager class will do lazy init and will query all mounted
   // filesystems. This leads to deadlock when we re-enter our mounted fuse fs.
   // Once initialized it seems to work fine.
-  NSFileManager* fileManager = [NSFileManager defaultManager];
-  [fileManager contentsOfDirectoryAtPath:@"/Volumes" error:NULL];
+  NSFileManager* fileManager = [[[NSFileManager alloc] init] autorelease];
+  [fileManager contentsOfDirectoryAtPath:@"/Volumes" error:nil];
 
   NSMutableArray* arguments =
     [NSMutableArray arrayWithObject:[[NSBundle mainBundle] executablePath]];
